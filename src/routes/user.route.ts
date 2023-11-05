e71defdfcd339f051e2a25e77b72fae5806e2901
jole141 <josip.jurenic12@gmail.com>
2023-11-05 03:47:32 +0100

    add protected endpoints

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@interfaces/routes.interface';
 import { Router } from 'express';
 import UserController from '@controllers/user.controller';
+import bearerTokenValidationMiddleware from '@middlewares/bearerTokenValidation.middleware';
 
 class UserRoute implements Routes {
   public path = '/user';
@@ -12,11 +13,11 @@ class UserRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}/:id`, this.userController.getUserById);
-    this.router.post(`${this.path}/addCar/:id`, this.userController.addCar);
-    this.router.post(`${this.path}/removeCar/:id`, this.userController.removeCar);
-    this.router.post(`${this.path}/addBalance/:id`, this.userController.addBalance);
-    this.router.post(`${this.path}/deductBalance/:id`, this.userController.deductBalance);
+    this.router.get(`${this.path}/:id`, bearerTokenValidationMiddleware, this.userController.getUserById);
+    this.router.post(`${this.path}/addCar/:id`, bearerTokenValidationMiddleware, this.userController.addCar);
+    this.router.post(`${this.path}/removeCar/:id`, bearerTokenValidationMiddleware, this.userController.removeCar);
+    this.router.post(`${this.path}/addBalance/:id`, bearerTokenValidationMiddleware, this.userController.addBalance);
+    this.router.post(`${this.path}/deductBalance/:id`, bearerTokenValidationMiddleware, this.userController.deductBalance);
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import IndexRoute from '@routes/index.route';
 import ParkingSpotRoute from '@routes/parkingSpot.route';
 import ParkingClusterRoute from '@routes/parkingCluster.route';
 import AuthRoute from '@routes/auth.route';
+import UserRoute from '@routes/user.route';
 
-const app = new App([new IndexRoute(), new ParkingSpotRoute(), new ParkingClusterRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new ParkingSpotRoute(), new ParkingClusterRoute(), new AuthRoute(), new UserRoute()]);
 
 app.listen();
